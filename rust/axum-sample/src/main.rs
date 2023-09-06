b146a45d61b919081b14c456dd577c25a624867d
ksktada <ksk.tada@gmail.com>
2023-09-06 23:59:59 +0900

    add graceful shutdown

--- rust/axum-sample/src/main.rs
+++ rust/axum-sample/src/main.rs
@@ -3,6 +3,8 @@ use axum::{
     Router,
 };
 
+use tokio::signal;
+
 #[tokio::main]
 async fn main() {
     // build our application with a single route
@@ -11,6 +13,33 @@ async fn main() {
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("signal received, starting graceful shutdown");
 }
