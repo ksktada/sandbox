5e5e15a81847277ad39669fc1446c023504d93c3
ksktada <ksk.tada@gmail.com>
2023-09-17 13:18:53 +0900

    fmt

--- microservices-demo-rust/online_boutique/productcatalogservice/usecase/src/lib.rs
+++ microservices-demo-rust/online_boutique/productcatalogservice/usecase/src/lib.rs
@@ -6,16 +6,14 @@ use std::fs::File;
 use std::io::BufReader;
 
 fn parse_catalog() -> Products {
-    let file_name = "products.json";
-    let file = File::open(file_name).unwrap();
+    let file = File::open("products.json").unwrap();
     let reader = BufReader::new(file);
     let products: Products = serde_json::from_reader(reader).unwrap();
     products
 }
 
 pub fn list_products() -> Vec<ProductDto> {
-    let products = parse_catalog();
-    products
+    parse_catalog()
         .products
         .into_iter()
         .map(|e| e.into())
@@ -23,17 +21,18 @@ pub fn list_products() -> Vec<ProductDto> {
 }
 
 pub fn get_product(product_id: &String) -> Option<ProductDto> {
-    let products = parse_catalog();
-    let product = products.products.into_iter().find(|e| &e.id == product_id);
-    match product {
+    match parse_catalog()
+        .products
+        .into_iter()
+        .find(|e| &e.id == product_id)
+    {
         Some(p) => Some(p.into()),
         None => None,
     }
 }
 
 pub fn search_products(query: &String) -> Option<Vec<ProductDto>> {
-    let products = parse_catalog();
-    let results = products
+    let results = parse_catalog()
         .products
         .into_iter()
         .filter(|e| {
