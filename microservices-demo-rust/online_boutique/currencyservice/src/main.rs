bb606a5bca5f16e5e31264ec9ebc9a7816e62a33
ksktada <ksk.tada@gmail.com>
2023-09-18 19:21:56 +0900

    start currencyservice impl

--- microservices-demo-rust/online_boutique/currencyservice/src/main.rs
+++ microservices-demo-rust/online_boutique/currencyservice/src/main.rs
@@ -1,3 +1,15 @@
+use std::{collections::HashMap, fs::File, io::BufReader};
+
+type CurrencyCode = String;
+type Rate = String;
+
+fn parse_currency_conversion() -> HashMap<CurrencyCode, Rate> {
+    let file = File::open("currency_conversion.json").unwrap();
+    let reader = BufReader::new(file);
+    serde_json::from_reader(reader).unwrap()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let c = parse_currency_conversion();
+    println!("{:?}", c);
 }
