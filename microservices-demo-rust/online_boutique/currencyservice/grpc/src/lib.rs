9c3f613f665acb00d2fa06462851e6cd6356192e
ksktada <ksk.tada@gmail.com>
2023-09-18 22:35:32 +0900

    fix to ddd like

--- microservices-demo-rust/online_boutique/currencyservice/grpc/src/lib.rs
+++ microservices-demo-rust/online_boutique/currencyservice/grpc/src/lib.rs
@@ -12,6 +12,7 @@ use online_boutique::{
 use tonic::transport::server::Router;
 use tonic::{transport::Server, Request, Response, Status};
 use usecase::dto::MoneyDto;
+use usecase::{get_supported_currencies, convert};
 
 // dto -> pbの変換を実装
 impl From<MoneyDto> for Money {
@@ -24,6 +25,17 @@ impl From<MoneyDto> for Money {
     }
 }
 
+// pb -> dtoの変換を実装
+impl Into<MoneyDto> for Money {
+    fn into(self) -> MoneyDto {
+        MoneyDto {
+            currency_code: self.currency_code,
+            units: self.units,
+            nanos: self.nanos,
+        }
+    }
+}
+
 // pbのserviceを実装
 #[derive(Debug, Default)]
 struct CurrencyServiceImpl {}
@@ -31,17 +43,15 @@ struct CurrencyServiceImpl {}
 #[tonic::async_trait]
 impl CurrencyService for CurrencyServiceImpl {
     async fn get_supported_currencies(&self, _request: Request<Empty>,) -> Result<Response<GetSupportedCurrenciesResponse>, Status> {
-        let currency_codes = vec!["USD".to_string()];
+        let currency_codes = get_supported_currencies().await;
         Ok(Response::new(GetSupportedCurrenciesResponse { currency_codes }))
     }
 
-    async fn convert(&self, _request: Request<CurrencyConversionRequest>) -> Result<Response<Money>, Status> {
-        let money = Money {
-            currency_code: "USD".to_string(),
-            units: 1,
-            nanos: 9000
-        };
-        Ok(Response::new(money))
+    async fn convert(&self, request: Request<CurrencyConversionRequest>) -> Result<Response<Money>, Status> {
+        let from = &request.get_ref().from.clone().unwrap().into();
+        let to_code = &request.get_ref().to_code;
+        let money = convert(from, to_code).await;
+        Ok(Response::new(money.into()))
     }
 }
 
--- microservices-demo-rust/online_boutique/currencyservice/usecase/src/lib.rs
+++ microservices-demo-rust/online_boutique/currencyservice/usecase/src/lib.rs
@@ -2,9 +2,18 @@ pub mod dto;
 
 use std::{collections::HashMap, fs::File, io::BufReader};
 use domain::r#type::{CurrencyCode, Rate};
+use dto::MoneyDto;
 
 fn parse_currency_conversion() -> HashMap<CurrencyCode, Rate> {
     let file = File::open("currency_conversion.json").unwrap();
     let reader = BufReader::new(file);
     serde_json::from_reader(reader).unwrap()
+}
+
+pub async fn get_supported_currencies() -> Vec<CurrencyCode> {
+    parse_currency_conversion().into_keys().collect::<Vec<CurrencyCode>>()
+}
+
+pub async fn convert(from: &MoneyDto, to_code: &CurrencyCode) -> MoneyDto {
+    todo!()
 }
