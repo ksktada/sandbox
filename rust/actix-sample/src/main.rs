6580e2b6cdc2b12adf247af47de9205007165d43
ksktada <ksk.tada@gmail.com>
2023-10-07 23:59:59 +0900

    add state

--- rust/actix-sample/src/main.rs
+++ rust/actix-sample/src/main.rs
@@ -1,5 +1,10 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+// This struct represents state
+struct AppState {
+    app_name: String,
+}
+
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -14,21 +19,36 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+// fn for scope
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("scope/index")
+}
+
+// fn for state
+#[get("/state")]
+async fn state(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; // <- get app_name
+    format!("Hello {app_name}!") // <- response with app_name
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            // basic
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            // scope
             .service(
                 web.scope("/scope")
                 .route("index", web::get().to(scope))
             )
+            // state
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web"),
+            }))
+            .service(state)
     })
     .shutdown_timeout(60)
     .workers(2)
