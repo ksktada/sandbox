ebf1bf9cf7214e42576b52b48d4d1a817796f2ae
ksktada <ksk.tada@gmail.com>
2023-10-15 13:05:04 +0900

    add tutorial

--- python/fastapi/main.py
+++ python/fastapi/main.py
@@ -1,25 +1,60 @@
-from fastapi import FastAPI
+from typing import Union
+from fastapi import FastAPI, Form
 from pydantic import BaseModel
 
+# uvicorn main:app --reload
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 class Item(BaseModel):
     name: str
+    description: Union[str, None] = None
     price: float
-    is_offer: bool = None
+    tax: Union[float, None] = None
 
-
+# get data simply
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
 
+# path parameter and query parameter
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+# query parameter
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+# json request
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+# form request
+@app.post("/items2/")
+async def create_item2(
+    name: str = Form(), price: str = Form(), description: Union[str, None] = Form(), tax: Union[float, None] = Form()
+):
+    item = Item(name=name, description=description, price=price, tax=tax)
+    return item
