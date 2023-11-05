0152db0f5a6966c077078d2bc5f3b3956c2a38f9
ksktada <ksk.tada@gmail.com>
2023-11-05 18:22:54 +0900

    add json generator by python

---
+++ python/json_data_generation/main.py
@@ -0,0 +1,17 @@
+import json
+import random, string
+
+# https://qiita.com/Scstechr/items/c3b2eb291f7c5b81902a
+def randomname(n):
+   return ''.join(random.choices(string.ascii_letters + string.digits, k=n))
+
+number_of_fields = 10
+length_of_data = 10
+data = {}
+dir = './data.json'
+
+for i in range(1, number_of_fields+1):
+    data['field'+str(i)] = randomname(length_of_data)
+
+with open(dir, mode="wt", encoding="utf-8") as f:
+	json.dump(data, f, ensure_ascii=False, indent=2)
