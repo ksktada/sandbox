4b49f9590428c60a7d06f51b50e31245d8db98f6
ksktada <ksk.tada@gmail.com>
2023-11-05 18:23:28 +0900

    add comment

--- python/json_data_generation/main.py
+++ python/json_data_generation/main.py
@@ -13,5 +13,6 @@ dir = './data.json'
 for i in range(1, number_of_fields+1):
     data['field'+str(i)] = randomname(length_of_data)
 
+# https://qiita.com/Mijinko/items/d37d069e5a7485a72b11
 with open(dir, mode="wt", encoding="utf-8") as f:
 	json.dump(data, f, ensure_ascii=False, indent=2)
