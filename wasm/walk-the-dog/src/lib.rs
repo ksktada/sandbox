e957da857c4d0422c20cddcda7c8828120d44943
ksktada <ksk.tada@gmail.com>
2023-11-27 23:59:59 +0900

    triangle

--- wasm/walk-the-dog/src/lib.rs
+++ wasm/walk-the-dog/src/lib.rs
@@ -1,6 +1,5 @@
 use wasm_bindgen::prelude::*;
-use web_sys::console;
-
+use web_sys;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -10,12 +9,33 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+    console_error_panic_hook::set_once();
+
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+    context.move_to(300.0, 0.0); // top of triangle
+    context.begin_path();
+    context.line_to(0.0, 600.0); // bottom left of triangle
+    context.line_to(600.0, 600.0); // bottom right of triangle
+    context.line_to(300.0, 0.0); // back to top of triangle
+    context.close_path();
+    context.stroke();
+    context.fill();
 
     Ok(())
 }
