fff3d214bf8794bdb45adae468ca1ade9a82ecc7
ksktada <ksk.tada@gmail.com>
2024-02-28 23:59:59 +0900

    fix

--- rust/asynchronous/src/main.rs
+++ rust/asynchronous/src/main.rs
@@ -1,32 +1,8 @@
-// https://github.com/ProgrammingRust/examples/blob/master/cheapo-request/src/main.rs
-
-use async_std::io::prelude::*;
-use async_std::net;
+// https://github.com/oreilly-japan/conc_ytakano/blob/main/chap5/5.2/ch5_2_2_sched/src/main.rs
 use asynchronous::{Executor, Hello};
 
-async fn cheapo_request(host: &str, port: u16, path: &str) -> std::io::Result<String> {
-    let mut socket = net::TcpStream::connect((host, port)).await?;
-
-    let request = format!("GET {} HTTP/1.1\r\nHost: {}\r\n\r\n", path, host);
-    socket.write_all(request.as_bytes()).await?;
-    socket.shutdown(net::Shutdown::Write)?;
-
-    let mut response = String::new();
-    socket.read_to_string(&mut response).await?;
-
-    Ok(response)
-}
-
 fn main() {
-    // use async_std::task;
-
-    // let response = task::block_on(cheapo_request("example.com", 80, "/"))?;
-    // println!("{}", response);
-
-    // Asynchronous Task Execution
     let executor = Executor::new();
     executor.get_spawner().spawn(Hello::new());
     executor.run();
-
-    // Ok(())
 }
