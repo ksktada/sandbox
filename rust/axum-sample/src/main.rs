efc39aea9e1a05614d18610e85e1fa298fa26ab8
ksktada <ksk.tada@gmail.com>
2024-02-29 23:59:59 +0900

    add router

--- rust/axum-sample/src/main.rs
+++ rust/axum-sample/src/main.rs
@@ -1,4 +1,11 @@
-use axum::{routing::get, Router};
+// https://github.com/tokio-rs/axum
+
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
 use tokio::signal;
 
 #[tokio::main]
@@ -7,7 +14,10 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, Root!" }))
+        .route("/hello", get(hello))
+        .route("/users", post(create_user));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -15,6 +25,39 @@ async fn main() {
         .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
+}
+
+async fn hello() -> &'static str {
+    "Hello, World!"
+}
+
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
 }
 
 async fn shutdown_signal() {
